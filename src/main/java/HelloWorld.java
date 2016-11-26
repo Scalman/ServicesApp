f537fe1c870fc7028bff3af78c18b54b8b1b8aea
marthel <marthel@kth.se>
2016-11-26 02:07:41 +0100

    asdddwdwd

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,16 +1,11 @@
 import DB.DAL.UserDb;
 import DB.Entities.UserEntity;
-import com.sun.xml.internal.ws.api.ha.StickyFeature;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.Collection;
-
-/**
- * Created by waleedhassan on 26/11/16.
- */
 
 
 @Path("/get")
